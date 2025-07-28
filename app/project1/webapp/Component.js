eae0c54df558ed563930ab843365856307d4f8a7
AC208289U12 AC208289U12 <ac208289u12@sapexperienceacademy.com>
2025-07-28 17:58:43 +0000

    still fixing blank page

--- app/project1/webapp/Component.js
+++ app/project1/webapp/Component.js
@@ -1,12 +1,15 @@
-sap.ui.define(
-    ["sap/fe/core/AppComponent"],
-    function (Component) {
-        "use strict";
-
-        return Component.extend("project1.Component", {
-            metadata: {
-                manifest: "json"
-            }
-        });
-    }
-);
+sap.ui.define([
+    "sap/ui/core/UIComponent"
+], function(UIComponent) {
+    "use strict";
+    return UIComponent.extend("project1.Component", {
+        metadata: {
+            manifest: "json",
+            interfaces: ["sap.ui.core.IAsyncContentCreation"]
+        },
+        init: function() {
+            UIComponent.prototype.init.apply(this, arguments);
+            this.getRouter().initialize();
+        }
+    });
+});
